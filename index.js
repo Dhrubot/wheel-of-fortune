fe7377ca6b8489b226ff19543554fe49a65372b5
Dhrubot <dhrubo.dh@gmail.com>
2020-11-23 22:43:29 -0500

    Added comments to clarify the actions more

--- index.js
+++ index.js
@@ -4,18 +4,28 @@
     let deg = 0
 
     startButton.addEventListener('click', () => {
+        // Disable button during spin
         startButton.style.pointerEvents = 'none'
-        deg = Math.floor(5000 + Math.random() * 5000)
+        // Calculate a new rotation between 3000 and 6000
+        deg = Math.floor(3000 + Math.random() * 3000)
+        // Set the transition on the wheel
         wheel.style.transition = 'all 8s ease-out'
+        // Rotate the wheel
         wheel.style.transform = `rotate(${deg}deg)`
+        // the blur effect to make it look like blurry
         wheel.classList.add('blur')
     })
 
     wheel.addEventListener('transitionend', () => {
         wheel.classList.remove('blur')
+        // Enable back the button when spin is over
         startButton.style.pointerEvents = 'auto'
+        // Need to set transition to none as we want to rotate instantly
         wheel.style.transition = 'none'
+        // At this point our degree is obviouly more than 360 as set in line 10. So we need to reset it to a number
+        // that is within 360 to have a natural spin.
         const actualDeg = deg %  360
+        // Start the next spin from a value less then 360
         wheel.style.transform = `rotate(${actualDeg}deg)`
     })
 })()
