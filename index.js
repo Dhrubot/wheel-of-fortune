becb52600b720b2166a55685e6619472688dcbb6
Dhrubot <dhrubo.dh@gmail.com>
2020-11-23 20:02:17 -0500

    added functionality to wheel the spin using the add eventlistener from the start button using wheel transition and degree

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 (function() {
-    const wheel = document.querySelector('wheel')
+    const wheel = document.querySelector('.wheel')
     const startbutton = document.querySelector('.button')
     let deg = 0
 
     startbutton.addEventListener('click', () => {
-
+        startbutton.style.pointerEvents = 'none'
+        deg = Math.floor(5000 + Math.random() * 5000)
+        wheel.style.transition = 'all 10s ease-out'
+        wheel.style.transform = `rotate(${deg}deg)`
     })
 
     wheel.addEventListener('transitionend', () => {
-        
+
     })
 })()
