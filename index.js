92dd01756ced3dc414b127b9f6a1231006ad7673
Dhrubot <dhrubo.dh@gmail.com>
2020-11-23 19:54:18 -0500

    Added js file to the project, started on the button event listener

---
+++ index.js
@@ -0,0 +1,13 @@
+(function() {
+    const wheel = document.querySelector('wheel')
+    const startbutton = document.querySelector('.button')
+    let deg = 0
+
+    startbutton.addEventListener('click', () => {
+
+    })
+
+    wheel.addEventListener('transitionend', () => {
+        
+    })
+})()
